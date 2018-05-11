228ff473de33ea8be9baa6cd2a5ad1924d682165
Sergei Valevka <sergei.valevka@gresearch.co.uk>
2018-05-11 13:56:20 +0000

    Initial Commit

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -1,6 +1,6 @@
 #
-# Cookbook:: demorole
+# Cookbook:: build_cookbook
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-include_recipie 'baserole::default'
+include_recipe 'delivery-truck::default'
